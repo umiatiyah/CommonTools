a643da854bf6599b8b41cba524ef3bfff3b6e216
umiatiyah <umiatiyah97@gmail.com>
2023-01-15 17:44:49 +0700

    add console app mail sender

---
+++ MailSender/Program.cs
@@ -0,0 +1,63 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleMailSender
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("---Start---");
+
+            string fullName = "manusia";
+            string email = "examplemailumi@gmail.com";
+
+            try {
+                // read template email and parameter
+                string html = string.Format(File.ReadAllText("templates/send_email.html"),
+                    fullName,
+                    email
+                    );
+
+                // read configuration in appsettings.json
+                var builder = new ConfigurationBuilder()
+                    .AddJsonFile($"appsettings.json", true, true);
+                var config = builder.Build();
+                
+                string? hostEmail = config["Config:Email:Host"];
+                int portEmail = Convert.ToInt32(config["Config:Email:Port"]);
+                string? usernameEmail = config["Config:Email:Username"];
+                string? passwordEmail = config["Config:Email:Password"];
+                string? fromEmail = config["Config:Email:From"];
+                string? fromName = config["Config:Email:FromName"];
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress(fromName, fromEmail));
+                mimeMessage.To.Add(new MailboxAddress(fullName, email));
+                mimeMessage.Subject = "MeetMe";
+                mimeMessage.Body = new TextPart("html")
+                {
+                    Text = html
+                };
+
+                // set SmtpClient
+                using var client = new SmtpClient();
+                await client.ConnectAsync(hostEmail, portEmail, false);
+                await client.AuthenticateAsync(usernameEmail, passwordEmail);
+                await client.SendAsync(mimeMessage);
+                await client.DisconnectAsync(true);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.ToString()}");
+            }
+
+            Console.WriteLine("Sent Success!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MailSender/README.md
@@ -0,0 +1,23 @@
+# Console App Simple Mail Sender
+
+
+## Description
+Console App send email with SmtpClient
+
+## Technologies
+Project is created with:
+* Console App .net 7
+
+## Tools
+* VS Code
+
+## Setup
+To run this project:
+
+```
+$ dotnet run
+```
+(need create solution and add this project to run in Visual Studio)
+
+## Credits
+* Template Email : https://www.designedwithbee.com
